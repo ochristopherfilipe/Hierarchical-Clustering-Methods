6e6af1a7a574d61bc27706f11a4fcfe3227aff0e
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2023-12-11 22:45:40 -0400

    Update app_clustering.py

--- app_clustering.py
+++ app_clustering.py
@@ -106,17 +106,11 @@ Nosso objetivo agora é agrupar as sessões de acesso ao portal considerando o c
                 st.markdown("---")
         
                 st.subheader("Padronizando as colunas")
-
-                @st.cache_data
-                def criar_df_auxiliar(df):
-                    df_pad = pd.DataFrame()
-                    df_pad[variaveis_qtd] = df[variaveis_qtd]
-                    df_pad = pd.concat([df_pad, pd.get_dummies(df[variaveis_cat], drop_first=True)], axis=1)
-                    return df_pad
-
-                
-                # Criar DataFrame auxiliar
-                df_pad = criar_df_auxiliar(df)
+        
+                df_pad = pd.DataFrame()
+                df_pad[variaveis_qtd] = df[variaveis_qtd]
+                df_pad = pd.concat([df_pad, pd.get_dummies(df[variaveis_cat], drop_first=True)], axis=1)        
+    
 
                 # Exibir nomes das colunas
                 st.write("Nomes das Colunas no DataFrame Transformado:")
